f19781881978973397d423efbf409bb006d70da3
lerkor-online <lerkor.online@gmail.com>
2023-12-07 20:37:17 -0300

    viewport fix 4

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 export const metadata = {
   title: 'Lerkor-dev',
   description: 'Front-end Developer',
-  viewport: 'width=device-width, initial-scale=1, viewport-fit=contain',
+  viewport: 'width=device-width, height=device-height, initial-scale=1, viewport-fit=contain',
 }
 
 export default function RootLayout({ children }) {
