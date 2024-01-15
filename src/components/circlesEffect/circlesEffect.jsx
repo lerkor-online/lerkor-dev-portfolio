a298165c4b06ff03fc075e518ae9c670470b5f7d
lerkor-online <lerkor.online@gmail.com>
2024-01-15 06:28:20 -0300

    fix z-index

--- src/components/circlesEffect/circlesEffect.jsx
+++ src/components/circlesEffect/circlesEffect.jsx
@@ -3,7 +3,7 @@ import './circlesEffect.css'
 
 const CirclesEffect = () => {
   return (
-    <div className='fixed top-0 left-0 w-full h-full z-[2]'>
+    <div className='fixed top-0 left-0 w-full h-full'>
         <ul className='circles'>
         <li></li>
         <li></li>
