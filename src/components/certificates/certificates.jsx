65a525da7d4c3d7938fe1577fc5ec212d9de6d73
lerkor-online <lerkor.online@gmail.com>
2024-01-15 06:31:31 -0300

    fix z-index 2

--- src/components/certificates/certificates.jsx
+++ src/components/certificates/certificates.jsx
@@ -4,7 +4,7 @@ import Slider666 from '@/components/slider666/slider666';
 const Certificates = () => {
   return (
     <div className='h-screen flex justify-center items-center bg-light-whitecustom dark:bg-light-blackcustom'>
-      <div className='w-screen'>
+      <div className='w-screen relative z-10'>
         <h1 className='pb-3 text-center text-4xl items-center justify-center text-light-blackcustom dark:text-light-whitecustom mb-6 section-title'>CERTIFICADOS</h1>
         <Slider666 />
       </div>
